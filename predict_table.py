f98b574696444b1649388c46481ebf5201262b79
zhangxinnan <zhangxin19870504@163.com>
2019-11-18 09:25:59 +0800

    predict_zx.py

---
+++ predict_table.py
@@ -0,0 +1,163 @@
+import argparse
+import os
+
+import numpy as np
+from PIL import Image, ImageDraw
+from keras.applications.vgg16 import preprocess_input
+from keras.preprocessing import image
+from keras.utils import plot_model
+
+import cfg
+from label import point_inside_of_quad
+from network import East
+from nms import nms
+from preprocess import resize_image
+
+
+def sigmoid(x):
+    """`y = 1 / (1 + exp(-x))`"""
+    return 1 / (1 + np.exp(-x))
+
+
+def cut_text_line(geo, scale_ratio_w, scale_ratio_h, im_array, img_path, s):
+    geo /= [scale_ratio_w, scale_ratio_h]
+    p_min = np.amin(geo, axis=0)
+    p_max = np.amax(geo, axis=0)
+    min_xy = p_min.astype(int)
+    max_xy = p_max.astype(int) + 2
+    sub_im_arr = im_array[min_xy[1]:max_xy[1], min_xy[0]:max_xy[0], :].copy()
+    for m in range(min_xy[1], max_xy[1]):
+        for n in range(min_xy[0], max_xy[0]):
+            if not point_inside_of_quad(n, m, geo, p_min, p_max):
+                sub_im_arr[m - min_xy[1], n - min_xy[0], :] = 255
+    sub_im = image.array_to_img(sub_im_arr, scale=False)
+    sub_im.save(img_path + '_subim%d.jpg' % s)
+
+
+def predict(east_detect, img_path, pixel_threshold, out_dir='_tmp/', img_size=736, quiet=False):
+    img = image.load_img(img_path)
+    img_filename = os.path.basename(img_path)
+    # 256
+    # d_wight, d_height = resize_image(img, cfg.max_predict_img_size)
+    d_wight, d_height = resize_image(img, img_size)
+    img = img.resize((d_wight, d_height), Image.NEAREST).convert('RGB')
+    img = image.img_to_array(img)
+    img = preprocess_input(img, mode='tf')
+    x = np.expand_dims(img, axis=0)
+    y = east_detect.predict(x)
+
+    y = np.squeeze(y, axis=0)
+    y[:, :, :3] = sigmoid(y[:, :, :3])
+    cond = np.greater_equal(y[:, :, 0], pixel_threshold)
+    activation_pixels = np.where(cond)
+    quad_scores, quad_after_nms = nms(y, activation_pixels)
+    with Image.open(img_path) as im:
+        im_array = image.img_to_array(im.convert('RGB'))
+        # d_wight, d_height = resize_image(im, cfg.max_predict_img_size)
+        d_wight, d_height = resize_image(im, img_size)
+        scale_ratio_w = d_wight / im.width
+        scale_ratio_h = d_height / im.height
+        im = im.resize((d_wight, d_height), Image.NEAREST).convert('RGB')
+        quad_im = im.copy()
+        draw = ImageDraw.Draw(im)
+        for i, j in zip(activation_pixels[0], activation_pixels[1]):
+            px = (j + 0.5) * cfg.pixel_size
+            py = (i + 0.5) * cfg.pixel_size
+            line_width, line_color = 1, 'red'
+            if y[i, j, 1] >= cfg.side_vertex_pixel_threshold:
+                if y[i, j, 2] < cfg.trunc_threshold:
+                    line_width, line_color = 2, 'yellow'
+                elif y[i, j, 2] >= 1 - cfg.trunc_threshold:
+                    line_width, line_color = 2, 'green'
+            draw.line([(px - 0.5 * cfg.pixel_size, py - 0.5 * cfg.pixel_size),
+                       (px + 0.5 * cfg.pixel_size, py - 0.5 * cfg.pixel_size),
+                       (px + 0.5 * cfg.pixel_size, py + 0.5 * cfg.pixel_size),
+                       (px - 0.5 * cfg.pixel_size, py + 0.5 * cfg.pixel_size),
+                       (px - 0.5 * cfg.pixel_size, py - 0.5 * cfg.pixel_size)],
+                      width=line_width, fill=line_color)
+        # im.save(img_path + '_act_zx_%d.jpg' % img_size)
+        im.save(os.path.join(out_dir, img_filename + '_act_zx_%d.jpg' % img_size))
+        quad_draw = ImageDraw.Draw(quad_im)
+        txt_items = []
+        for score, geo, s in zip(quad_scores, quad_after_nms,
+                                 range(len(quad_scores))):
+            if np.amin(score) > 0:
+                quad_draw.line([tuple(geo[0]),
+                                tuple(geo[1]),
+                                tuple(geo[2]),
+                                tuple(geo[3]),
+                                tuple(geo[0])], width=2, fill='red')
+                if cfg.predict_cut_text_line:
+                    cut_text_line(geo, scale_ratio_w, scale_ratio_h, im_array,
+                                  img_path, s)
+                rescaled_geo = geo / [scale_ratio_w, scale_ratio_h]
+                rescaled_geo_list = np.reshape(rescaled_geo, (8,)).tolist()
+                txt_item = ','.join(map(str, rescaled_geo_list))
+                txt_items.append(txt_item + '\n')
+            elif not quiet:
+                print('quad invalid with vertex num less then 4.')
+        # quad_im.save(img_path + '_predict_zx_%d.jpg' % img_size)
+        quad_im.save(os.path.join(out_dir, img_filename + '_predict_zx_%d.jpg' % img_size))
+        if cfg.predict_write2txt and len(txt_items) > 0:
+            with open(img_path[:-4] + '_' + str(img_size) + '.txt', 'w') as f_txt:
+                f_txt.writelines(txt_items)
+
+
+def predict_txt(east_detect, img_path, txt_path, pixel_threshold, quiet=False):
+    img = image.load_img(img_path)
+    d_wight, d_height = resize_image(img, cfg.max_predict_img_size)
+    scale_ratio_w = d_wight / img.width
+    scale_ratio_h = d_height / img.height
+    img = img.resize((d_wight, d_height), Image.NEAREST).convert('RGB')
+    img = image.img_to_array(img)
+    img = preprocess_input(img, mode='tf')
+    x = np.expand_dims(img, axis=0)
+    y = east_detect.predict(x)
+
+    y = np.squeeze(y, axis=0)
+    y[:, :, :3] = sigmoid(y[:, :, :3])
+    cond = np.greater_equal(y[:, :, 0], pixel_threshold)
+    activation_pixels = np.where(cond)
+    quad_scores, quad_after_nms = nms(y, activation_pixels)
+
+    txt_items = []
+    for score, geo in zip(quad_scores, quad_after_nms):
+        if np.amin(score) > 0:
+            rescaled_geo = geo / [scale_ratio_w, scale_ratio_h]
+            rescaled_geo_list = np.reshape(rescaled_geo, (8,)).tolist()
+            txt_item = ','.join(map(str, rescaled_geo_list))
+            txt_items.append(txt_item + '\n')
+        elif not quiet:
+            print('quad invalid with vertex num less then 4.')
+    if cfg.predict_write2txt and len(txt_items) > 0:
+        with open(txt_path, 'w') as f_txt:
+            f_txt.writelines(txt_items)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--path', '-p',
+                        default='demo/012.png',
+                        help='image path')
+    parser.add_argument('--out_dir', default='_tmp/')
+    # pixel_threshold = 0.9
+    parser.add_argument('--threshold', '-t',
+                        default=cfg.pixel_threshold,
+                        help='pixel activation threshold')
+    parser.add_argument('--weight_file', default='saved_model/east_model_weights_3T736.h5')
+    parser.add_argument('--img_size', default=736, type=int)
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    img_path = args.path
+    threshold = float(args.threshold)
+    print(img_path, threshold)
+
+    east = East()
+    east_detect = east.east_network()
+    plot_model(east_detect, to_file='east_model.png')
+    # east_detect.load_weights(cfg.saved_model_weights_file_path)
+    east_detect.load_weights(args.weight_file)
+    predict(east_detect, img_path, threshold, img_size=args.img_size, out_dir=args.out_dir)
